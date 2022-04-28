736eb7e6dfd1e8e2de6fc748f20f74b601a582e8
mickmorrison <mmorrisonau@gmail.com>
2022-04-28 16:08:58 +1000

    included FOR loop
    Bugs. Loop not looping. Requires smarts.

--- austcems_scraper.R
+++ austcems_scraper.R
@@ -1,15 +1,17 @@
 # Austcems_scraper
 # A simple scraper for the Australian Cemeteries Index website
 # https://austcemindex.com/
-# Michael Morrison
-# 7 April 2022
 
 # Load libraries
 library(tidyverse)
 library(rvest)
 
+# Do a loop to get all the pages of results
+for(pages in 1:20){
+  
 # Create a new variable containing the URL of the search results
-url <- read_html("https://austcemindex.com/?cemetery=1211&page=2")
+link = paste0("https://austcemindex.com/?cemetery=1211&page=", pages)
+url <- read_html(link)
 
 # create a new variable listing the tables in the HTML
 tables <- url %>% html_table(fill=TRUE)
@@ -19,4 +21,4 @@ data <- (tables[[2]])
 
 # export the data variable containing cemetery inscriptions as a csv file
 write.csv(data,"~/ownCloud/UNE/Teaching/HINQ302 Researching the Past in the Digital Age/Dev/austcems/test_lg_cem.csv")
-
+}
